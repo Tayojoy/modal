17711106df2058e6b16af00d39fbd362529e7d7d
Tayo Joy <zaccheaus2013@gmail.com>
2022-06-12 01:17:27 +0100

    modal cose

--- script.js
+++ script.js
@@ -3,26 +3,34 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal')
-//console.log(btnOpenModal);
+console.log(btnOpenModal);
 
-//for (let i = 0; i < btnOpenModal.length; i++); {
-  //  btnOpenModal[i].addEventListener('click', function () {
-    //    console.log('button clicked');
-      //  modal.classList.remove('hidden');
-        //overlay.classList.remove('hidden')
-    //})
-//}
-//btnOpenModal[i].addEventListener('click', function () {
- //   console.log('button clicked');
-   // modal.classList.remove('hidden');
-  //  overlay.classList.remove('hidden')
-//})
-
-function addup (n) {
-    let sum = 0
-    for (let i = 0; i <= n; i++); {
-        sum += i;
-    }
-    return sum
+const openModal =function () {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+     overlay.classList.remove('hidden')
+ }
+for (let i = 0; i < btnOpenModal.length; i++) {
+   btnOpenModal[i].addEventListener('click', openModal)
 }
-console.log(addup(10));
+// btnOpenModal[i].addEventListener('click', function () {
+//    console.log('button clicked');
+//    modal.classList.remove('hidden');
+//    overlay.classList.remove('hidden')
+// })
+
+// function addup (n) {
+//     let sum = 0
+//     for (let i = 0; i <= n; i++) {
+//         sum += i;
+//     }
+//     return sum
+// }
+// console.log(addup(10));
+const closeModal =  function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden')
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
